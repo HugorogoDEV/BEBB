ada1b346e81053bd3c613a23b0cb5f04d1c8c5da
Hugo <hugorogonzagaconta@gmail.com>
2023-08-08 20:56:08 -0300

    Banco de Dados funcionando corretamente!

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import { Text, View, Image, StyleSheet, Dimensions, TextInput, TouchableOpacity } from 'react-native';
 import { saveLoginData, getLoginData } from './database';
-import SQLite from 'react-native-sqlite-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const HomeScreen = ({}) => {
+export const HomeScreen = ({ }) => {
 
   const [nome, setnome] = useState('');
   const [dose, setdose] = useState('');
-
   const [input1, setInput1] = useState('');
   const [input2, setInput2] = useState('');
   const [input3, setInput3] = useState('');
   const [input4, setInput4] = useState('');
 
+  // Função para segurar a ação de salvar
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem('input1', input1);
+      await AsyncStorage.setItem('input2', input2);
+      await AsyncStorage.setItem('input3', input3);
+      await AsyncStorage.setItem('input4', input4);
+      await AsyncStorage.setItem('nome', nome);
+      await AsyncStorage.setItem('dose', dose);
+      console.log(nome)
+    } catch (error) {
+      Alert.alert('There was an error saving the data.');
+    }
+  };
 
   return (
     <View style={[styles.container, styles.backgroundBranco]}>
@@ -20,8 +33,8 @@ export const HomeScreen = ({}) => {
         <View style={styles.fileira}>
           <View style={styles.colunaTopo}>
             <View style={styles.container}><Text style={styles.titulo}>Bora {'\n'}Bebber </Text></View>
-            <View style={styles.container}><Text style={styles.subTitulo}>Como quer{'\n'}ser chamado?</Text><TextInput onChangeText={(text)=> setnome(text)} value={nome} placeholderTextColor="blue"  placeholder="Seu Nome" /></View>
-            <View style={styles.container}><Text style={[styles.subTitulo]}>Quanto de água{'\n'}vai tomar{'\n'}por dose?</Text><TextInput onChangeText={(text)=> setdose(text)} value={dose} placeholderTextColor='blue' placeholder='300ml' /></View>
+            <View style={styles.container}><Text style={styles.subTitulo}>Como quer{'\n'}ser chamado?</Text><TextInput onChangeText={(text) => setnome(text)} placeholderTextColor="blue" placeholder="Seu Nome" /></View>
+            <View style={styles.container}><Text style={[styles.subTitulo]}>Quanto de água{'\n'}vai tomar{'\n'}por dose?</Text><TextInput onChangeText={(text) => setdose(text)} placeholderTextColor='blue' placeholder='300ml' /></View>
           </View>
           <View style={styles.colunaTopo}>
             <View style={[styles.container, styles.centralizar]}><Text style={styles.inputSubTitulo}>Precisamos{'\n'}de alguns dados{'\n'}para começarmos{'\n'}anos hidratar</Text></View>
@@ -29,21 +42,21 @@ export const HomeScreen = ({}) => {
               <View style={styles.fileira}>
                 <View style={styles.coluna}>
                   <Text style={[styles.subTitulo, styles.input]}>Idade</Text>
-                  <TextInput onChangeText={(text) => setInput1(text)} value={input1} keyboardType='numeric' placeholder='input1'/>
+                  <TextInput onChangeText={(text) => setInput1(text)} placeholder='input1' />
                   <Text style={[styles.subTitulo, styles.input]}>Peso</Text>
-                  <TextInput onChangeText={(text) => setInput2(text)} value={input2} keyboardType='numeric' placeholder='input2'/>
+                  <TextInput onChangeText={(text) => setInput2(text)} placeholder='input2' />
                 </View>
                 <View style={styles.coluna}>
                   <Text style={[styles.subTitulo, styles.input]}>Altura</Text>
-                  <TextInput onChangeText={(text) => setInput3(text)} value={input3} keyboardType='numeric' placeholder='input3'/>
+                  <TextInput onChangeText={(text) => setInput3(text)} placeholder='input3' />
                   <Text style={[styles.subTitulo, styles.input]}>Sexo</Text>
-                  <TextInput onChangeText={(text) => setInput4(text)} value={input4} keyboardType='numeric' placeholder='input4'/>
+                  <TextInput onChangeText={(text) => setInput4(text)} placeholder='input4' />
                 </View>
               </View>
             </View>
             <View style={styles.container}>
-              <TouchableOpacity style={[styles.button,styles.centralizar]} onPress={saveLoginData}>
-                <Text style={{color: 'white', fontSize: 20, textAlign: 'center'}}>Salvar</Text>
+              <TouchableOpacity style={[styles.button, styles.centralizar]} onPress={saveData}>
+                <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>Salvar</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -52,45 +65,6 @@ export const HomeScreen = ({}) => {
       </View>
     </View>
   );
-};
-
-
-const db = SQLite.openDatabase(
-  { name: 'MainDB', location: 'default' },
-  () => {},
-  error => console.log(error),
-);
-
-db.transaction((tx) => {
-  tx.executeSql(
-    'CREATE TABLE IF NOT EXISTS Person (id INTEGER PRIMARY KEY AUTOINCRENT, name TEXT, age INTEGER, height FLOAT, weight FLOAT)',
-  );
-});
-
-const insertPerson = (name, age, height, weight) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      'INSERT INTO Person (name, age, height, weight) VALUES (?, ?, ?, ?)',
-      [name, age, height, weight],
-    );
-  });
-};
-
-const updatePerson = (id, name, age, height, weight) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      'UPDATE Person SET name=?, age=?, height=?, weight=?, WHERE id=?',
-      [name, age, height, weight, id],
-    );
-  });
-};
-
-const getPersons = (callback) => {
-  db.transaction((tx) => {
-    tx.executeSql('SELECT * FROM Person', [], (_, results) => {
-      callback(results.rows.raw());
-    });
-  });
 };
 
 const width = Dimensions.get('screen').width;
@@ -112,7 +86,7 @@ export const styles = StyleSheet.create({
     flexDirection: 'column',
   },
   colunaTopo: {
-    
+
     flexDirection: 'column',
     padding: 25,
   },
